db94ebb999770012263839eefa6a11b15cc351b1
PRASANNA07-11-2005 <imraghu0805@gmail.com>
2025-06-24 11:42:50 +0530

    commit

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 import axios from "axios";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// import dotenv from 'dotenv';
-// dotenv.config(); // Load .env variables
+
 
 const app = express();
 const port = 4000;
 console.log(port)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.static("public"));
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
@@ -112,6 +106,6 @@ app.get("/", async (req, res) => {
   }
 
 })
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("Server listening on port 4000");
 })
